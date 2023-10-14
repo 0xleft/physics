b563a806e94e02aa27c5ba8db2b540e65aa7c53a
AdomasDauda <107749872+0xleft@users.noreply.github.com>
2023-10-14 12:23:49 +0100

    Initial commit

---
+++ app/Main.hs
@@ -0,0 +1,6 @@
+module Main (main) where
+
+import Lib (calcUarmOverTime, calcUarm)
+
+main :: IO ()
+main = calcUarmOverTime 1 (-6.0) 42.0 2.8 0
---
+++ src/Lib.hs
@@ -0,0 +1,38 @@
+module Lib(calcUarm, calcUarmOverTime) where
+import Text.Printf (printf)
+import Control.Monad (unless)
+import Control.Exception (handle)
+import System.IO (openFile, hGetContents, hClose, IOMode(ReadMode, ReadWriteMode, WriteMode), hPutStrLn)
+import Data.List.Split
+import GHC.IO.Handle.Types (HandleType(ReadWriteHandle))
+
+g :: Double
+g = 9.81
+
+calcUarm :: Double -> Double -> Double -> Double -> Double
+calcUarm init_angle init_velocity init_height time = do
+    let y = -(g/(2*(init_velocity**2 * cos init_angle ** 2))) * time**2 + (tan init_angle * time) + init_height
+    y
+
+calcUarmOverTime :: Double -> Double -> Double -> Double -> Double -> IO ()
+calcUarmOverTime step init_angle init_velocity init_height time = do
+    let y = calcUarm init_angle init_velocity init_height time
+    -- printf "x: %.2f y: %.2f\n" time y
+    -- printf "%.2f\n" init_angle
+
+    graphUarmToTime time y
+    unless (y <= 0) $ calcUarmOverTime step init_angle init_velocity init_height (time + step)
+
+insertCharAtStart :: Char -> String -> String
+insertCharAtStart char str = char : str
+
+isEmptyList :: [a] -> Bool
+isEmptyList xs = case xs of
+  [] -> True
+  _  -> False
+
+graphUarmToTime :: Double -> Double -> IO ()
+graphUarmToTime x y = do
+    let new_line = replicate (round y) ' ' ++ "o"
+
+    print new_line
